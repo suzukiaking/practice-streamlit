a3724cca8fbaba244d9cae7e08f3447ddfbc52a6
Akira Suzuki <yukimidaifukunigatedesu@gmail.com>
2021-12-26 15:31:11 +0900

    2nd commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.write('プログレスバーの表示')
 'Start!!'
 
 latest_iteration = st.empty()
-bar = st.progress()
+bar = st.progress(0)
 for i in range(100):
   latest_iteration.text(f'Iteration {i + 1}')
   bar.progress(i + 1)
